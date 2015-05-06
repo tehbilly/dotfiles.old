1c3cc27b90cb6560a76a99b680ed003997385941
William McGann <contact@WilliamMcGann.com>
2015-05-05 20:10:16 -0400

    Aria2 handy dandy shortcuts

--- source/10_aliases.sh
+++ source/10_aliases.sh
@@ -6,6 +6,11 @@ alias find="find ${@} 2>/dev/null" # This stops find from flooding stderr for pe
 # Docker aliases
 alias docker-clean="docker rmi \$(docker images | awk '{if (\$2 == \"<none>\") print \$3;}')"
 
+# Aria2 aliases
+if [[ "$(type -P aria2c)" ]]; then
+    alias get='aria2c'
+fi
+
 # Do we have an identity file? Probably want to use it.
 if [[ -e ~/.ssh/id_rsa ]]; then
     alias ssh="ssh -i ~/.ssh/id_rsa"
